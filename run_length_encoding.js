88265ed77a38e8b24e5aff6b17b7d608309e2f4b
hridhya <menonhridhya@gmail.com>
2015-10-02 16:05:19 -0700

    RLS

---
+++ run_length_encoding.js
@@ -0,0 +1,16 @@
+function RunLength(str) {
+
+    str = str.split("");
+    var count = 1;
+    var answer = "";
+
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] === str[i + 1]) {
+            count++;
+        } else {
+            answer += count.toString() + str[i].toString();
+            count = 1;
+        }
+    }
+    return answer;
+}
