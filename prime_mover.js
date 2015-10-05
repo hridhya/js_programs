67f03ecb71a77d834d1aec0cacb92c5edc738e72
hridhya <menonhridhya@gmail.com>
2015-10-05 13:03:01 -0700

    prime mover

---
+++ prime_mover.js
@@ -0,0 +1,23 @@
+function PrimeMover(mynum) {
+
+    function test(num) {
+        var isPrime = true;
+        for (var i = 2; i < num; i++) {
+            if (num % i === 0) {
+                isPrime = false;
+            }
+        }
+        return isPrime;
+    }
+
+    var primeArr = [2];
+    var cur = 3;
+    
+    while (primeArr.length < mynum) {
+        if (test(cur)) {
+            primeArr.push(cur);
+        }
+        cur++;
+    }
+    return primeArr.pop();
+}
